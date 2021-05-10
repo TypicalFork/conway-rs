c495753558b662d43207dc25dd1f77015750ad45
Klim Tsoutsman <klimusha@gmail.com>
2021-05-10 12:00:59 +1000

    Move display function into main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
-use conway::display::display;
-use std::sync::{atomic::AtomicBool, Arc};
+use conway::game::State;
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    thread, time,
+};
+use termion::{
+    clear,
+    screen::{ToAlternateScreen, ToMainScreen},
+};
 
 fn main() {
     let terminate = Arc::new(AtomicBool::new(false));
     signal_hook::flag::register(signal_hook::consts::SIGINT, Arc::clone(&terminate)).unwrap();
-    display(Arc::clone(&terminate));
+
+    print!("{}", ToAlternateScreen);
+    let mut state = State::random(0.2, (67, 40));
+
+    while !terminate.load(Ordering::Relaxed) {
+        print!("{}", clear::All);
+        print!("{}", state);
+
+        thread::sleep(time::Duration::from_millis(200));
+        state = state.next();
+    }
+
+    print!("{}", ToMainScreen);
 }
